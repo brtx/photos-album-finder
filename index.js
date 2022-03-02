8ed455d0f39bad2ba77920a15541adfe8d71ed62
Stefan Barthel <stefan.barthel@zuehlke.com>
2022-03-02 12:41:27 -0500

    switched to better-sqlite3, as sqlite3 wasn't working with node v16. fixed db schema to match current schema (on my mac). fixed package name.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const sqlite3 = require('sqlite3').verbose();
+const db = require('better-sqlite3')(
+  '/Users/bas/Pictures/Photos Library.photoslibrary/database/photos.sqlite', 
+  // { verbose: console.log }
+);
 var osa = require('osa2')
 
 function getSelectedPhotoId() {
@@ -16,43 +19,35 @@ getSelectedPhotoId()
     const slashIndex = selectedPhotoId.indexOf("/");
     const rootId = selectedPhotoId.substring(0, slashIndex);
 
-    let db = new sqlite3.Database('/Users/bas/Pictures/Photos Library.photoslibrary/database/photos.sqlite', (err) => {
-      if (err) {
-        console.error(err.message);
-      }
-      console.log('Connected to the photos database.');
-    });
+    // console.log(rootId);
 
     let i = 0;
-    db.serialize(() => {
-      db.each(`
-        SELECT ZADDITIONALASSETATTRIBUTES.Z_PK as pkAdd, 
-               ZASSET.Z_PK as pkAss,
-               ZORIGINALFILENAME as filename, 
-               ZGENERICALBUM.ZTITLE as albumTitle, 
-               ZALTERNATEIMPORTIMAGEDATE as fileCreatedDate, 
-               ZASSET.ZDATECREATED as date, 
-               ZGENERICALBUM.ZTITLE, 
-               ZASSET.ZKIND, 
-               ZASSET.ZDURATION, 
-               ZASSET.ZUNIFORMTYPEIDENTIFIER as videoType,
-               ZASSET.ZUUID as id
-        FROM ZADDITIONALASSETATTRIBUTES
-        INNER JOIN ZASSET
-        ON ZADDITIONALASSETATTRIBUTES.Z_PK = ZASSET.ZADDITIONALATTRIBUTES
-        INNER JOIN Z_26ASSETS
-        ON ZASSET.Z_PK = Z_26ASSETS.Z_3ASSETS
-        INNER JOIN ZGENERICALBUM
-        ON Z_26ASSETS.Z_26ALBUMS = ZGENERICALBUM.Z_PK
-        WHERE ZASSET.ZUUID = "${rootId}"
-        `, (err, row) => {
-          if (err) {
-            console.error(err.message);
-          }
-          // console.log(i++ + "\t" + row.pkAss + "\t" + row.pkAdd + "\t" + row.filename + "\t" + row.albumTitle + "\t" + row.fileCreatedDate + "\t" + row.date + "\t" + row.videoType + "\t" + row.id);
-          console.log(i++ + "\t" + row.albumTitle);
-      });
-    });
+    const statement = db.prepare(`
+    SELECT ZADDITIONALASSETATTRIBUTES.Z_PK as pkAdd, 
+           ZASSET.Z_PK as pkAss,
+           ZORIGINALFILENAME as filename, 
+           ZGENERICALBUM.ZTITLE as albumTitle, 
+           ZALTERNATEIMPORTIMAGEDATE as fileCreatedDate, 
+           ZASSET.ZDATECREATED as date, 
+           ZGENERICALBUM.ZTITLE, 
+           ZASSET.ZKIND, 
+           ZASSET.ZDURATION, 
+           ZASSET.ZUNIFORMTYPEIDENTIFIER as videoType,
+           ZASSET.ZUUID as id
+    FROM ZADDITIONALASSETATTRIBUTES
+    INNER JOIN ZASSET
+    ON ZADDITIONALASSETATTRIBUTES.Z_PK = ZASSET.ZADDITIONALATTRIBUTES
+    INNER JOIN Z_27ASSETS
+    ON ZASSET.Z_PK = Z_27ASSETS.Z_3ASSETS
+    INNER JOIN ZGENERICALBUM
+    ON Z_27ASSETS.Z_27ALBUMS = ZGENERICALBUM.Z_PK
+    WHERE ZASSET.ZUUID = '${rootId}'
+    `);
+
+    for (const row of statement.iterate()) {
+      // console.log(i++ + "\t" + row.pkAss + "\t" + row.pkAdd + "\t" + row.filename + "\t" + row.albumTitle + "\t" + row.fileCreatedDate + "\t" + row.date + "\t" + row.videoType + "\t" + row.id);
+      console.log(i++ + "\t" + row.albumTitle);
+    }
 
     db.close((err) => {
       if (err) {
